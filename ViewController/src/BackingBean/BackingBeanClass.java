da1acdbbb9b906530315b0908a9a79c4c62d4bf1
Dinesh Kumar <dineshkb@thoughtworks.com>
2014-09-01 12:39:22 +0530

    Removed Unnecessary files and View callind Rest URL

--- ViewController/src/BackingBean/BackingBeanClass.java
+++ ViewController/src/BackingBean/BackingBeanClass.java
@@ -3,12 +3,35 @@ package BackingBean;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 
+import com.sun.jersey.api.client.*;
+
 public class BackingBeanClass {
     public BackingBeanClass() {
     }
 
-    public void it1_validator(FacesContext facesContext, UIComponent uIComponent, Object object) {        
+    public void it1_validator(FacesContext facesContext,
+                              UIComponent uIComponent, Object object) {
         String value = object.toString();
         System.out.println(value);
+    }
+
+    public String onSubmit() {
+
+        Client client = Client.create();
+        String serverURL = "http://localhost:7101/WebService-WebserviceTest-context-root/jersey/user";
+        WebResource webResource =
+            client.resource(serverURL);
+        ClientResponse response;
+        response =  
+                webResource.accept("application/json").get(ClientResponse.class);
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : " +
+                                       response.getStatus());
+        }
+
+        String output = response.getEntity(String.class);
+        System.out.println("Output from Server .... \n");
+        System.out.println(output);
+        return "Success";
     }
 }
